ac9dbb732bf74e4283b2231ef1655ad7749481b4
Simone Chiaretta <simone@piyosailing.com>
2016-11-16 21:42:22 +0100

    Implemented del and list

--- dotnet-prop/Program.cs
+++ dotnet-prop/Program.cs
@@ -2,27 +2,42 @@
 using Microsoft.Build.Evaluation;
 using Microsoft.Extensions.CommandLineUtils;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 class Program
 {
+    const string VERSION = "VersionPrefix";
+    const string FRAMEWORK = "TargetFramework";
+    const string RUNTIME_IDENTIFIERS = "RuntimeIdentifiers";
+
     static void Main(string[] args)
     {
+        //Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", @"C:\Program Files (x86)\dotnet\sdk\1.0.0-preview3-004056\MSBuild.dll");
         new Program().Run(args);
     }
 
     public void Run(string[] args)
     {
-        var app = new CommandLineApplication();
-        app.Name = "dotnet prop";
-        app.FullName = ".NET propject parameter utility";
-        app.Description = "Sets and unsets parameters in the Csproj file";
 
+        //RemoveProperty("Version");
+        //return;
+
+        var app = new CommandLineApplication()
+        {
+            Name = "dotnet prop",
+            FullName = ".NET project parameter utility",
+            Description = "Sets and unsets parameters in the Csproj file"
+        };
         app.HelpOption("-?|-h|--help");
 
         app.Command("add", command => {
             command.Description = "Adds a parameter to the project file";
 
-            var versionOption = command.Option("-v|--version <version>", "Set version number", CommandOptionType.SingleValue);
+            var versionOption = command.Option("-v|--version <VERSION>", "Set version number", CommandOptionType.SingleValue);
             var runtimesOption = command.Option("-r|--runtime <RUNTIME_IDENTIFIERS>", "Set supported runtimes", CommandOptionType.MultipleValue);
             var frameworkOption = command.Option("-f|--framework <FRAMEWORK>", "Set supported framework", CommandOptionType.SingleValue);
             command.HelpOption("-?|-h|--help");
@@ -35,22 +50,25 @@ class Program
                 }
                 if (versionOption.HasValue())
                 {
-                    AddOrUpdateProperty("VersionPrefix", versionOption.Value());
-                    Console.WriteLine($"Setting VersionPrefix to {versionOption.Value()}");
+                    AddOrUpdateProperty(VERSION, versionOption.Value());
+                    Console.WriteLine($"Setting {VERSION} to {versionOption.Value()}");
                 }
-                //if (runtimesOption.HasValue())
-                //{
-                //    foreach ()
-                //    {
 
-                //    }
-                //    AddOrUpdateProperty("RuntimeIdentifiers", versionOption.Value());
-                //    Console.WriteLine($"Setting VersionPrefix to {versionOption.Value()}");
-                //}
+                if (runtimesOption.HasValue())
+                {
+                    StringBuilder builder = new StringBuilder();
+                    foreach (var option in runtimesOption.Values)
+                    {
+                        builder.Append($"{option};");
+                    }
+                    var runtimeString = builder.ToString().TrimEnd(';');
+                    AddOrUpdateProperty(RUNTIME_IDENTIFIERS, runtimeString);
+                    Console.WriteLine($"Setting {RUNTIME_IDENTIFIERS} to {runtimeString}");
+                }
                 if (frameworkOption.HasValue())
                 {
-                    AddOrUpdateProperty("TargetFramework", frameworkOption.Value());
-                    Console.WriteLine($"Setting TargetFramework to {frameworkOption.Value()}");
+                    AddOrUpdateProperty(FRAMEWORK, frameworkOption.Value());
+                    Console.WriteLine($"Setting {FRAMEWORK} to {frameworkOption.Value()}");
                 }
 
                 return 0;
@@ -60,12 +78,49 @@ class Program
 
         app.Command("del", command => {
             command.Description = "Removes a parameter from the project file";
+            var versionOption = command.Option("-v|--version", "Removes version number", CommandOptionType.NoValue);
+            var runtimesOption = command.Option("-r|--runtime", "Removes supported runtimes", CommandOptionType.NoValue);
+            //var frameworkOption = command.Option("-f|--framework", "Removes supported framework", CommandOptionType.NoValue);
             command.HelpOption("-?|-h|--help");
+
+
+            command.OnExecute(() => {
+                if (!versionOption.HasValue() && !runtimesOption.HasValue() && !frameworkOption.HasValue())
+                {
+                    command.ShowHelp();
+                    return 2;
+                }
+                if (versionOption.HasValue())
+                {
+                    RemoveProperty(VERSION);
+                }
+
+                if (runtimesOption.HasValue())
+                {
+                    RemoveProperty(RUNTIME_IDENTIFIERS);
+                }
+                if (frameworkOption.HasValue())
+                {
+                    RemoveProperty(FRAMEWORK);
+                }
+
+                return 0;
+            });
+
         });
 
         app.Command("list", command => {
-            command.Description = "Lists all parameters from the  project file";
-            command.HelpOption("-?|-h|--help");
+            command.Description = "Lists all parameters from the project file";
+
+            command.OnExecute(()=> {
+                var msProj = GetMSBuildProject();
+                foreach (var item in msProj.Properties)
+                {
+                    Console.WriteLine($" - {item.Name} = {item.Value}");
+                }
+                return 0;
+            });
+
         });
 
         app.OnExecute(() =>
@@ -91,10 +146,38 @@ class Program
         msProj.Save();
     }
 
+    private void RemoveProperty(string propertyName)
+    {
+        var msProj = GetMSBuildProject();
+
+        var property = msProj.Properties.SingleOrDefault(p => p.Name == propertyName);
+
+        if (property == null)
+        {
+            Console.WriteLine($"No property {propertyName} in the file");
+            return;
+        }
+
+        Console.WriteLine($"Removing {propertyName}");
+        property.Parent.RemoveChild(property);
+
+        msProj.Save();
+    }
+
     private ProjectRootElement GetMSBuildProject()
     {
-        //TODO: fix absolule url :)
+        DirectoryInfo currDir = new DirectoryInfo(Directory.GetCurrentDirectory());
+        FileInfo projectFile = currDir.GetFiles("*.csproj").FirstOrDefault();
+
+        if (!projectFile.Exists)
+        {
+            throw new Exception("Unable to find any .csproj file in the current folder");
+        }
+
+        Console.WriteLine($"Updating file {projectFile.FullName}");
+
         return ProjectRootElement.Open(
+            //projectFile.FullName,
             @"C:\Users\user\Documents\GitHub\dotnet-prop\ConsumerApp\ConsumerApp.csproj",
             ProjectCollection.GlobalProjectCollection
             );
